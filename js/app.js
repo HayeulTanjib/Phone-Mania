ded5242d1a2876dfa0c9ae922723aa0d304bdf90
HayeulTanjib <tanjib.cse@gmail.com>
2022-03-01 00:55:03 +0600

    Display Searched Phones

--- js/app.js
+++ js/app.js
@@ -13,6 +13,45 @@ const loadPhoneData = async() => {
 }
 
 
+const displayPhone = (phones) => {
+    const phoneContainer = document.getElementById('phone_container');
+
+        //clear previous search
+        phoneContainer.textContent = '';
+
+     phones.forEach(phone => {
+    const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = 
+    `
+    <div class="card h-100 text-center">
+    <img src="${phone.image}" class="card-img-top mx-auto w-50 pt-3" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${phone.phone_name}</h5>
+      <p class="card-text">${phone.brand}</p>
+      <button>
+    </div>
+  </div>
+    `;
+
+    phoneContainer.appendChild(div);
+ });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
