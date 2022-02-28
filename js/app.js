1db3be20714a7f56bdd7cace62cf30b5bfaa03d3
HayeulTanjib <tanjib.cse@gmail.com>
2022-03-01 01:27:33 +0600

    Single Phone Details Show

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ const displayPhone = (phones) => {
     <div class="card-body">
       <h5 class="card-title">${phone.phone_name}</h5>
       <p class="card-text">${phone.brand}</p>
-      <button>
+      <button onclick="loadSinglePhoneData('${phone.slug}')" class="btn btn-primary">See Details</button>
     </div>
   </div>
     `;
@@ -40,8 +40,41 @@ const displayPhone = (phones) => {
 
 
 
+const loadSinglePhoneData = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const getData = await res.json();
+    displaySinglePhone(getData.data);
+}
 
 
+const displaySinglePhone = (phones) => {
+    const singlePhoneContainer = document.getElementById('singlePhone_container');
+    singlePhoneContainer.textContent = '';
+
+        const div = document.createElement('div');
+         div.classList.add('card');
+         div.style.width = '24rem';
+         div.style.textAlign = 'center';
+            
+        div.innerHTML = 
+            `
+            <img src="${phones.image}" class="card-img-top w-50 mx-auto mt-3" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${phones.name}</h5>
+              <p class="card-text">${phones.brand}</p>
+              <p class="card-text">${phones.releaseDate}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">An item</li>
+              <li class="list-group-item">A second item</li>
+              <li class="list-group-item">A third item</li>
+            </ul>
+            `;
+
+            singlePhoneContainer.appendChild(div);
+
+}
 
 
 
